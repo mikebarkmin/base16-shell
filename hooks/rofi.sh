c991d69286c5e556da0f8fda50cfc5aacfea9f8d
Mike Barkmin <mike.barkmin@uni-due.de>
2018-06-20 13:58:29 +0200

    change rofi hook

--- hooks/rofi.sh
+++ hooks/rofi.sh
@@ -3,7 +3,6 @@ rofi_config_file=$HOME/.local/share/rofi/themes/base16.rasi
 rofi_themes_dir=$HOME/.config/base16-rofi/themes
 
 rofi_theme_file=$rofi_themes_dir/base16-$BASE16_THEME.rasi
-echo $rofi_theme_file
 if [ -f $rofi_theme_file ]; then
   cat $rofi_theme_file > $rofi_config_file
   echo 'Rofi theme updated'
